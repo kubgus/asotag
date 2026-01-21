c397e81566567351cf622dbbae21d314bb3e365b
Jakub Guštafik <ja.github@gustafik.com>
2026-01-21 20:34:06 +0100

    refactor: format colors file

--- game/s-colors.go
+++ game/s-colors.go
@@ -5,16 +5,16 @@ import (
 )
 
 var (
-	ColReset    = utils.NewColor(utils.ColorReset)
+	ColReset = utils.NewColor(utils.ColorReset)
 
-	ColHealth   = utils.NewColor(utils.ColorFgBold, utils.ColorFgRed)
-	ColDamage   = utils.NewColor(utils.ColorFgBrightRed)
+	ColHealth = utils.NewColor(utils.ColorFgBold, utils.ColorFgRed)
+	ColDamage = utils.NewColor(utils.ColorFgBrightRed)
 
 	ColItem     = utils.NewColor(utils.ColorFgBold, utils.ColorFgBlue)
 	ColLocation = utils.NewColor(utils.ColorFgBold, utils.ColorFgItalic, utils.ColorFgWhite)
 	ColHero     = utils.NewColor(utils.ColorFgBold, utils.ColorFgCyan)
 
-	ColAction   = utils.NewColor(utils.ColorFgBold, utils.ColorFgYellow)
-	ColTooltip  = utils.NewColor(utils.ColorFgDim, utils.ColorFgWhite)
-	ColSystem   = utils.NewColor(utils.ColorFgBold, utils.ColorFgPurple)
+	ColAction  = utils.NewColor(utils.ColorFgBold, utils.ColorFgYellow)
+	ColTooltip = utils.NewColor(utils.ColorFgDim, utils.ColorFgWhite)
+	ColSystem  = utils.NewColor(utils.ColorFgBold, utils.ColorFgPurple)
 )
