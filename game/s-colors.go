7e98098fa2e5153d77b57d5ebc03ff2901b51731
Jakub Guštafik <ja.github@gustafik.com>
2026-01-21 20:30:16 +0100

    feat: update health and damage colors

---
+++ game/s-colors.go
@@ -0,0 +1,20 @@
+package game
+
+import (
+	"asotag/utils"
+)
+
+var (
+	ColReset    = utils.NewColor(utils.ColorReset)
+
+	ColHealth   = utils.NewColor(utils.ColorFgBold, utils.ColorFgRed)
+	ColDamage   = utils.NewColor(utils.ColorFgBrightRed)
+
+	ColItem     = utils.NewColor(utils.ColorFgBold, utils.ColorFgBlue)
+	ColLocation = utils.NewColor(utils.ColorFgBold, utils.ColorFgItalic, utils.ColorFgWhite)
+	ColHero     = utils.NewColor(utils.ColorFgBold, utils.ColorFgCyan)
+
+	ColAction   = utils.NewColor(utils.ColorFgBold, utils.ColorFgYellow)
+	ColTooltip  = utils.NewColor(utils.ColorFgDim, utils.ColorFgWhite)
+	ColSystem   = utils.NewColor(utils.ColorFgBold, utils.ColorFgPurple)
+)
